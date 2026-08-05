f36bd796aef34122f72d2eb8688a04479e687524
Mohamed Chaabani <141734857+hamma05@users.noreply.github.com>
2026-08-05 18:25:03 +0100

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -54,6 +54,7 @@ SESSION_COOKIE_HTTPONLY = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,6 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'project' / 'static',
+    BASE_DIR / 'staticfiles'
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
